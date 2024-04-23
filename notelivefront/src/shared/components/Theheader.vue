c63b18fbe641988e63e4c524432c36ac9bf9e846
APOgamer <adrianpalmaobi23@gmail.com>
2024-04-23 17:01:48 -0500

    add:header primer prototipo

--- notelivefront/src/shared/components/Theheader.vue
+++ notelivefront/src/shared/components/Theheader.vue
@@ -1,8 +1,8 @@
 <template>
   <header class="header-container">
-    <div class="header-content">
-      <!-- Aquí puedes agregar el contenido de tu encabezado -->
-    </div>
+    <img src="../../assets/logoAPPWEB.png" alt="logo" class="logo" />
+    <div class="spacer"></div>
+    <div class="username">Username</div>
   </header>
 </template>
 
@@ -15,15 +15,59 @@ export default {
 <style scoped>
 .header-container {
   background-color: #FFD8BA; /* Color de fondo del encabezado */
-  padding: 1.3vw; /* Espaciado interior del encabezado */
+  padding: 1vw; /* Espaciado interior del encabezado */
+  max-height: 3vw;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* Sombra del encabezado */
+  display: flex;
+  align-items: center; /* Para centrar verticalmente el contenido */
 }
 
-.header-content {
-  max-width: 100vw; /* Ancho máximo del contenido del encabezado */
+.username {
+  margin-right: 1vw;
 }
 
-.header-content h1 {
-  color: #333; /* Color del texto del encabezado */
+.spacer {
+  flex: 1 1 auto; /* Para ocupar todo el espacio disponible */
+}
+
+.logo {
+  height: 2vw;
+  border-radius: 5px; /* Reduzco el radio del borde para que no sea tan prominente */
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1); /* Añade una ligera sombra para dar profundidad */
+  margin-left: 1vw;
+  transition: transform 0.3s ease, box-shadow 0.3s ease; /* Transiciones suaves para transformación y sombra */
+}
+
+.logo:hover {
+  transform: scale(1.1); /* Aumenta ligeramente el tamaño al pasar el ratón por encima */
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); /* Añade una sombra más pronunciada */
+  animation: rotate 2s infinite alternate, moveUp 2s infinite alternate, changeColor 2s infinite alternate;
+}
+
+@keyframes rotate {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes moveUp {
+  from {
+    transform: translateY(0);
+  }
+  to {
+    transform: translateY(-5px);
+  }
+}
+
+@keyframes changeColor {
+  0% {
+    filter: hue-rotate(0deg);
+  }
+  100% {
+    filter: hue-rotate(360deg);
+  }
 }
 </style>

a0fb5ea577d621d9c7925992e9426304c74e2c44
erickpalomino1923 <erickpalomino0723@gmail.com>
2024-04-23 18:36:44 -0500

    update: Main-container CardPregunta

--- notelivefront/src/components/main-contaienr.vue
+++ notelivefront/src/components/main-contaienr.vue
@@ -1,16 +1,27 @@
 <template>
-<the-chat/>
+  <div>
+    <the-chat/>
+    <PreguntaCard :pregunta="pregunta" :nombre="nombre"></PreguntaCard>
+  </div>
 </template>
 
 <script>
 import TheChat from "@/shared/components/TheChat.vue";
+import PreguntaCard from "@/shared/components/PreguntaCard.vue";
 
 export default {
   name: 'main-container',
-  components: {TheChat}
-}
+  components: { TheChat, PreguntaCard },
+  data() {
+    return {
+      sidebarVisible: false,
+      pregunta: "¿Qué es la programación orientada a objetos?",
+      nombre: "Juan Pérez"
+    };
+  },
+};
 </script>
 
 <style scoped>
-
+/* Puedes agregar estilos aquí si es necesario */
 </style>

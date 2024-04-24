018db7d500e456ba887e1e13e5db25e8250aea51
APOgamer <adrianpalmaobi23@gmail.com>
2024-04-23 19:05:34 -0500

    Update App.vue

--- notelivefront/src/App.vue
+++ notelivefront/src/App.vue
@@ -22,23 +22,9 @@
     }
   }
   </script>
-
-<<<<<<< Updated upstream
-<style>
-#app {
-  font-family: Roboto,serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
-=======
   <style>
   #app {
     font-family: Roboto,serif;
 
   }
   </style>
->>>>>>> Stashed changes

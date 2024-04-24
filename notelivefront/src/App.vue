6cf46a7e78aa1798b5761b1736acce22f63abcae
erickpalomino1923 <erickpalomino0723@gmail.com>
2024-04-23 19:02:27 -0500

    update: corrección

--- notelivefront/src/App.vue
+++ notelivefront/src/App.vue
@@ -36,11 +36,7 @@
 <style>
 #app {
   font-family: Roboto,serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
 =======

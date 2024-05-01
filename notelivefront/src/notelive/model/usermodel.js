81b38a2752c08251420073f09e10e523ee90c3a4
APOgamer <adrianpalmaobi23@gmail.com>
2024-05-01 16:33:29 -0500

    Create usermodel.js

---
+++ notelivefront/src/notelive/model/usermodel.js
@@ -0,0 +1,8 @@
+export class Usuario {
+    constructor(id, nombre, tipo_plan, contraseña) {
+        this.id = id;
+        this.nombre = nombre;
+        this.tipo_plan = tipo_plan;
+        this.contraseña = contraseña;
+    }
+}

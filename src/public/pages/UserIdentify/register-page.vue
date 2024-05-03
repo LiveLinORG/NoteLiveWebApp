810d9d024724c6775de434445c6390cb2973de77
Piero Martinez <103530827+pmv612@users.noreply.github.com>
2024-05-02 22:50:42 -0500

    (feat): add register page

--- src/public/pages/UserIdentify/register-page.vue
+++ src/public/pages/UserIdentify/register-page.vue
@@ -1,22 +1,35 @@
 <script>
 import {defineComponent} from "vue";
-import TheHeader from "@/App.vue";
-import TheFooter from "@/App.vue";
+import RegisterForm from "@/components/register-form.component.vue";
 
 export default defineComponent({
-  components: {TheFooter, TheHeader}
+  components: {RegisterForm}
 })
 
 </script>
 
 <template>
-  <the-header/>
-  <div class="container-register-page">
-
+  <div class="general">
+  <h1 class="title">Registro</h1>
+    <register-form/>
   </div>
-  <the-footer/>
 </template>
 
 <style>
+.general{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  height: auto;
+  align-items: center;
+  margin-top: 1%;
+  margin-bottom: auto;
+}
 
+.title {
+  font-family: "Inter", sans-serif;
+  font-size: 48px;
+  margin-bottom: 0;
+}
 </style>
